9ab99d1719c5be8a5345a6b8f9580fcb4afaa927
hellomac87 <hellomac87@gmail.com>
2018-08-07 09:54:37 +0900

    Component

--- src/App.js
+++ src/App.js
@@ -17,7 +17,7 @@ class App extends Component {
     };
     return (
       <Fragment>
-        <MyComponent/>
+        <MyComponent name='React' age={3}/>
       </Fragment>
     );
   }
--- src/MyComponent.js
+++ src/MyComponent.js
@@ -5,18 +5,32 @@ class MyComponent extends Component {
     static defaultProps = {
         name : 'default name'
     }
+    static propTypes = {
+        name : PropTypes.string,
+        age : PropTypes.number.isRequired
+    }
+
+    state = {
+        number : 0
+    }
+    
     render() {
         return (
             <div>
-                Hello, My name is {this.props.name}
+                <p>Hello, My name is {this.props.name}</p>
+                <p>Hello, My age is {this.props.age}</p>
+                <p>this state number : {this.state.number}</p>
+                <button
+                    onClick={() => {
+                        this.setState({
+                            number: this.state.number + 1
+                        })
+                    }}>
+                    Plus
+                </button>
             </div>
         );
     }
 }
-
-MyComponent.propTypes = {
-    name : PropTypes.string
-}
-
 
 export default MyComponent;
