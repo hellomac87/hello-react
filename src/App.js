a5bb8c155ae96f67f376137bf1a4532b354b15e3
hellomac87 <hellomac87@gmail.com>
2018-08-08 16:36:30 +0900

    컴포넌트 스타일링 : css module

--- src/App.js
+++ src/App.js
@@ -1,24 +1,21 @@
 import React, { Component, Fragment } from 'react';
-import './App.css';
+import classNames from 'classnames/bind';
+import styles from'./App.css';
+
+const cx = classNames.bind(styles);
+
+console.log(styles);
 
 class App extends Component {
   render() {
-    const text = "are you awesome?"
-    const condition = true;
-    const style = {
-      backgroundColor : 'gray',
-      border: '1px solid black',
-      height: Math.round(Math.random() * 300) +50,
-      width : Math.round(Math.random() * 300) +50,
-      WebkitTransition : 'all',
-      MozTransition:'all',
-      msTransition :'all'
-    };
+    const isBlue = false;
     return (
       <Fragment>
-        <h1>Hello React</h1>
-        <h2>{ condition && 'true' }</h2>
-        <div style={style}></div>
+       <div className={cx('box',{
+         blue : isBlue
+       })}>
+
+       </div>
       </Fragment>
     );
   }
