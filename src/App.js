4d0ae86909ab287bc0f19dcc3dbc28bd7975120c
hellomac87 <hellomac87@gmail.com>
2018-08-08 16:57:55 +0900

    setting

--- src/App.js
+++ src/App.js
@@ -3,22 +3,9 @@ import './App.css';
 
 class App extends Component {
   render() {
-    const text = "are you awesome?"
-    const condition = true;
-    const style = {
-      backgroundColor : 'gray',
-      border: '1px solid black',
-      height: Math.round(Math.random() * 300) +50,
-      width : Math.round(Math.random() * 300) +50,
-      WebkitTransition : 'all',
-      MozTransition:'all',
-      msTransition :'all'
-    };
     return (
       <Fragment>
         <h1>Hello React</h1>
-        <h2>{ condition && 'true' }</h2>
-        <div style={style}></div>
       </Fragment>
     );
   }
